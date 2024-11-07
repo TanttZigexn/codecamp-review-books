6f725f0590df69da65556036f911b560397f62e0
TanttZigexn <130115369+TanttZigexn@users.noreply.github.com>
2024-11-07 10:51:38 +0700

    add workflow again

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   has_many :comments
   validates :title, presence: true
   validates :content, presence: true
+
+  scope :search, ->(query) {
+    where("title LIKE ? OR content LIKE ?", "%#{query}%", "%#{query}%")
+  }
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "popper", to: 'popper.js', preload: true
-pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "popper", to: "popper.js", preload: true
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
+pin_all_from "app/javascript/channels", under: "channels"
